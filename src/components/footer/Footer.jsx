4a18f8ea5afb791abd3caf9903d438e74d73a96a
Deborah Lyra <deborahp.lyra@gmail.com>
2024-12-20 15:05:02 -0300

    added footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import { FacebookLogo, InstagramLogo, TwitterLogo, Envelope, Phone } from 'phosphor-react';
+import logo from '../../../public/imgs/logo-semiramis.png'
+
+export function Footer() {
+    return (
+        <footer className="bg-roseTea text-gray-700 py-8">
+            <div className="container mx-auto flex items-center justify-around">
+
+                <div className="text-center md:text-left">
+                    <div className="flex justify-center md:justify-start gap-4">
+                        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                            <FacebookLogo size={32} weight="fill" color="#1877F2" />
+                        </a>
+                        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                            <InstagramLogo size={32} weight="fill" color="#E1306C" />
+                        </a>
+                        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                            <TwitterLogo size={32} weight="fill" color="#1DA1F2" />
+                        </a>
+                    </div>
+                </div>
+                <div className="text-center md:text-right">
+                    <img src={logo} alt="" className='h-24 md:h-16 lg:h-16'/>
+                </div>
+            </div>
+        </footer>
+    );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,15 +3,17 @@ import { Banner } from "../components/Banner/Banner";
 import { Books } from "../components/books/Books";
 import { Contact } from "../components/contact/Contact";
 import { Navbar } from "../components/Navbar/Navbar";
+import { Footer } from "../components/footer/Footer"
 
 export function HomePage() {
   return (
     <div className="h-screen">
-      <Navbar/>
-      <Banner/>
-      <AboutMe/>
-      <Books/>
-      <Contact/>
+      <Navbar />
+      <Banner />
+      <AboutMe />
+      <Books />
+      <Contact />
+      <Footer />
     </div>
   )
 }
