fab5c1396a503b2102f26b1d4d0ecf7d0a780bfa
Deborah Lyra <deborahp.lyra@gmail.com>
2024-12-29 12:06:24 -0300

    ...

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { Banner } from "../components/Banner/Banner";
 import { Books } from "../components/books/Books";
 import { Contact } from "../components/contact/Contact";
 import { Navbar } from "../components/Navbar/Navbar";
-import { Footer } from "../components/footer/Footer"
+
 
 export function HomePage() {
   return (
@@ -13,7 +13,7 @@ export function HomePage() {
       <AboutMe />
       <Books />
       <Contact />
-      <Footer />
+    
     </div>
   )
 }
